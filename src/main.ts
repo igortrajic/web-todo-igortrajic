431f3a0355b9ee456f3626d4e41b017f187ebf7a
igortrajic <iggetrajic@gmail.com>
2025-10-10 12:56:42 +0200

    feat: add tasks functionality closes #5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,38 @@
 import './style.css'
 
-console.log('Hello from typescript')
-console.log("attempting to fix git")
+const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
+const addButton = document.getElementById('add-todo-button')
+const todoElements = document.getElementById('todo-elements')
+const errorMessage = document.getElementById('error-message')
+function isError() {
+  if (errorMessage) {
+    errorMessage.textContent = 'Please enter a task!'
+  }
+}
+function clearError() {
+  if (errorMessage) {
+    errorMessage.textContent = ''
+  }
+}
+function addTodo() {
+  if (!todoInput || !todoElements) return
+  const task = todoInput.value
+  if (task === '') {
+    isError()
+  } else {
+    const newItem = document.createElement('li')
+    newItem.textContent = task
+    todoElements.appendChild(newItem)
+    todoInput.value = ''
+    clearError()
+  }
+}
+
+if (addButton && todoInput && todoElements) {
+  addButton.addEventListener('click', addTodo)
+  todoInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      addTodo()
+    }
+  })
+}
